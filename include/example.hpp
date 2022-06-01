d042e383f53ce9bde134417aac28b3d069a82be5
melnikova.m <92054042+melnikova-maria@users.noreply.github.com>
2022-06-01 19:51:34 +0300

    Update example.hpp

--- include/example.hpp
+++ include/example.hpp
@@ -1,8 +1,27 @@
-// Copyright 2021 Your Name <your_email>
+// Copyright 2021 Melnikova Maria dandelion.m@mail.ru
 
-#ifndef INCLUDE_EXAMPLE_HPP_
-#define INCLUDE_EXAMPLE_HPP_
+#ifndef INCLUDE_PROCESS_BUILDER_HPP_
+#define INCLUDE_PROCESS_BUILDER_HPP_
+#include <iostream>
+#include <string>
 
-auto example() -> void;
+#include <thread>
+#include <async++.h>
+#include <boost/process.hpp>
+#include <boost/program_options.hpp>
 
-#endif // INCLUDE_EXAMPLE_HPP_
+class process
+{
+ public:
+  process();
+  ~process();
+  void start_process(bool Install, bool Pack, std::string Config,
+                     int Time);
+  void timer(int time);
+  bool my_task(std::string task);
+  void out_help();
+ private:
+  boost::process::child new_child;
+};
+#endif // INCLUDE_PROCESS_BUILDER_HPP_
+
